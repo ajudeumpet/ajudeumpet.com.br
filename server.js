82adc3dc0e7427af9bffda4e6eb992ce6843a4d9
HenryQRM <henryqrm@gmail.com>
2018-04-15 11:00:02 -0300

    redirect https

--- server.js
+++ server.js
@@ -124,6 +124,14 @@ async function webService() {
     next();
   });
 
+  app.get('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+      res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      next();
+    }
+  });
+
   app.use('/', express.static(path.resolve(__dirname, 'build')));
 
   app.set('views', path.join(__dirname, 'build'));
